e3146ea5e4816d77b28aa5f366279a39e2b5bba4
Pawel Krawczyk <p.krawczyk@kainos.com>
2014-02-24 17:37:25 +0000

    reformat, add UTF8 support

--- src/org/owasp/Main.java
+++ src/org/owasp/Main.java
@@ -3,6 +3,7 @@ package org.owasp;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -10,11 +11,20 @@ import java.security.NoSuchAlgorithmException;
 public class Main {
 
     public static void main(String[] args)
-            throws IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException, NoSuchAlgorithmException, InvalidKeyException, InvalidAlgorithmParameterException {
-	StringEnvelope env = new StringEnvelope();
+            throws IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException,
+            NoSuchAlgorithmException, InvalidKeyException, InvalidAlgorithmParameterException,
+            UnsupportedEncodingException {
 
-    String wrapped = env.wrap("ss", "aaa");
-    System.out.println(wrapped);
-    env.unwrap(wrapped, "aaa");
+        final String key = "this is key";
+        final String plaintext = "this is the message";
+
+        StringEnvelope env = new StringEnvelope();
+
+        System.out.println("Plaintext=" + plaintext);
+        String wrapped = env.wrap(plaintext, key);
+
+        System.out.println("Encrypted=" + wrapped);
+
+        System.out.println("Decrypted=" + env.unwrap(wrapped, key));
     }
 }
